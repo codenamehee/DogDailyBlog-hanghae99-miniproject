9970bfdc3bd44f8a47a41de827623829a272f37f
codenamehee <jhyfordev@gmail.com>
2021-10-11 15:13:10 +0900

    initial commit

---
+++ src/main/java/com/hanghae/dogdailyblog_miniproject/DogdailyblogMiniprojectApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.dogdailyblog_miniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogdailyblogMiniprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogdailyblogMiniprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae/dogdailyblog_miniproject/DogdailyblogMiniprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.dogdailyblog_miniproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogdailyblogMiniprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
