3577819bb279ee4117346f28d8a64c4d8a7569d6
startas <83121954+startdata@users.noreply.github.com>
2021-10-12 15:27:44 +0900

    [FIX] : 로그인상태에서 게시글 작성

--- src/main/java/com/hanghae/dogdailyblog_miniproject/controller/ContentsController.java
+++ src/main/java/com/hanghae/dogdailyblog_miniproject/controller/ContentsController.java
@@ -3,8 +3,10 @@ package com.hanghae.dogdailyblog_miniproject.controller;
 import com.hanghae.dogdailyblog_miniproject.dto.ContentsRequestDto;
 import com.hanghae.dogdailyblog_miniproject.model.Contents;
 import com.hanghae.dogdailyblog_miniproject.repository.ContentsRepository;
+import com.hanghae.dogdailyblog_miniproject.security.UserDetailsImpl;
 import com.hanghae.dogdailyblog_miniproject.service.ContentsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,9 +33,25 @@ public class ContentsController {
     }
     //게시물 생성
     @PostMapping("/write")
-    public Contents createContents(@RequestBody ContentsRequestDto requestDto){
-        Contents contents = new Contents(requestDto);
-        return ContentsRepository.save(contents);
+    public Contents createContents(@RequestBody ContentsRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDatails){
+        //로그인 되어 있는 ID의 username
+        String username = userDatails.getUser().getUsername();
+        Contents contents = ContentsService.createContents(requestDto, username);
+        return contents;
+    }
+
+    //게시물 수정
+    @PutMapping("/write/{id}")
+    public Long updateContents(@PathVariable Long id, @RequestBody ContentsRequestDto requestDto){
+        ContentsService.update(id,requestDto);
+        return id;
+    }
+
+    //게시글 삭제
+    @DeleteMapping("/write/{id}")
+    public Long deletContents(@PathVariable Long id){
+        ContentsRepository.deleteById(id);
+        return id;
     }
 }
 
