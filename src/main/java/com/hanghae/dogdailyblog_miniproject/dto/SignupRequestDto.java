a384c80ddfbea8c021617c8bf6cb0bbedfdd34e4
codenamehee <jhyfordev@gmail.com>
2021-10-12 15:03:14 +0900

    [Add] : security cors설정, MySQL 연결, EC2 서버 연결

--- src/main/java/com/hanghae/dogdailyblog_miniproject/dto/SignupRequestDto.java
+++ src/main/java/com/hanghae/dogdailyblog_miniproject/dto/SignupRequestDto.java
@@ -9,4 +9,10 @@ public class SignupRequestDto {
     private String userid;
     private String username;
     private String pwd;
+
+    public SignupRequestDto(String userid, String username, String pwd) {
+        this.userid = userid;
+        this.username = username;
+        this.pwd = pwd;
+    }
 }
--- src/main/java/com/hanghae/dogdailyblog_miniproject/security/WebSecurityConfig.java
+++ src/main/java/com/hanghae/dogdailyblog_miniproject/security/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
 
         // 허용할 URL
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOrigin("http://localhost:3001");
         // 허용할 Header
         configuration.addAllowedHeader("*");
         // 허용할 Http Method
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // cors 설정
-        http.cors().configurationSource(corsConfigurationSource());
+        http.cors();
         http.csrf().disable();
         http.headers().frameOptions().disable();
         http.authorizeRequests()
@@ -59,12 +59,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 // 로그인 기능
                 .formLogin()
-                // 로그인 view 제공 (GET/user/login)
+//                // 로그인 view 제공 (GET/user/login)
                 .loginPage("/user/login")
-                // 로그인 처리 후 성공 시 URL
-                .defaultSuccessUrl("/")
-                // 로그인 처리 후 실패 시 URL
-                .failureUrl("user/login/error")
+//                // 로그인 처리 후 성공 시 URL
+//                .defaultSuccessUrl("/")
+//                // 로그인 처리 후 실패 시 URL
+//                .failureUrl("user/login?error")
                 .permitAll()
                 .and()
                 .logout()
