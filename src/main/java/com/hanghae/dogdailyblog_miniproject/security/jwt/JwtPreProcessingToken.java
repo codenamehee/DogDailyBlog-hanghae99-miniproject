643969170881cbdee5adcab1a693a5cc19d35197
ohchon <dtg8@naver.com>
2021-10-13 16:07:01 +0900

    [add]jwt

---
+++ src/main/java/com/hanghae/dogdailyblog_miniproject/security/jwt/JwtPreProcessingToken.java
@@ -0,0 +1,23 @@
+package com.hanghae.dogdailyblog_miniproject.security.jwt;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+public class JwtPreProcessingToken extends UsernamePasswordAuthenticationToken {
+
+    private JwtPreProcessingToken(
+            Object principal,
+            Object credentials
+    ) {
+        super(
+                principal,
+                credentials
+        );
+    }
+
+    public JwtPreProcessingToken(String token) {
+        this(
+                token,
+                token.length()
+        );
+    }
+}
