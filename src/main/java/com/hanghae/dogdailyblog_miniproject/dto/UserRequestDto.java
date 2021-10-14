c1cf5f2cdfed163a6c5bdd2b667f58419b61661d
ohchon <dtg8@naver.com>
2021-10-14 10:44:41 +0900

    [add] 토큰기능

---
+++ src/main/java/com/hanghae/dogdailyblog_miniproject/dto/UserRequestDto.java
@@ -0,0 +1,16 @@
+package com.hanghae.dogdailyblog_miniproject.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserRequestDto {
+
+    private String userName;
+    private String password;
+
+
+}
--- src/main/java/com/hanghae/dogdailyblog_miniproject/service/UserService.java
+++ src/main/java/com/hanghae/dogdailyblog_miniproject/service/UserService.java
@@ -1,49 +1,57 @@
 package com.hanghae.dogdailyblog_miniproject.service;
 
-import com.hanghae.dogdailyblog_miniproject.dto.SignupRequestDto;
+import com.hanghae.dogdailyblog_miniproject.exception.ApiRequestException;
 import com.hanghae.dogdailyblog_miniproject.model.User;
+import com.hanghae.dogdailyblog_miniproject.dto.SignupRequestDto;
+import com.hanghae.dogdailyblog_miniproject.dto.UserRequestDto;
+import com.hanghae.dogdailyblog_miniproject.exception.ApiRequestException;
 import com.hanghae.dogdailyblog_miniproject.repository.UserRepository;
+import com.hanghae.dogdailyblog_miniproject.security.JwtTokenProvider;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
+import javax.transaction.Transactional;
+import java.util.Optional;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
-//@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    //회원가입 유효성 체크
-    public Map<String, String> validateHandling(Errors errors) {
-        Map<String, String> validatorResult = new HashMap<>();
-
-        for(FieldError error : errors.getFieldErrors()) {
-            String validKeyName = String.format("valid_%s", error.getField());
-            validatorResult.put(validKeyName, error.getDefaultMessage());
-        }
-        return validatorResult;
-    }
-
-    // 회원가입 처리 로직
     public void registerUser(SignupRequestDto requestDto) {
-        String userid = requestDto.getUserid();
-        String username = requestDto.getUsername();
-        String pwd = passwordEncoder.encode(requestDto.getPwd());
 
-        User user = new User(userid, username, pwd);
+        String userid = requestDto.getUserid();
+
+        String username = requestDto.getUsername();
+
+        Optional<User> found = userRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new ApiRequestException("중복된 사용자 ID 가 존재합니다.");
+        }
+
+        String password = requestDto.getPwd();
+        password = passwordEncoder.encode(password);
+
+        User user = new User(userid, username, password);
         userRepository.save(user);
+
+    }
+
+    public String createToken(UserRequestDto user) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword());
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        return jwtTokenProvider.createToken(authentication);
     }
 }
+
